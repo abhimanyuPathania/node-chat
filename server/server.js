3305981e77232ec6aeadc893af497fa120e1b710
Abhimanyu Pathania <abpindia1944@gmail.com>
2017-02-12 19:35:14 +0530

    Add broadcasting

--- server/server.js
+++ server/server.js
@@ -11,20 +11,49 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+// io.on('connection', (socket) => {
+// 	console.log('New user connected');
+
+// 	socket.on('createMessage', (message) => {
+// 		console.log('createMessage', message);
+
+// 		// socket emits event to a single connection
+// 		// io will emit them to every connection
+// 		io.emit('newMessage', {
+// 			from: message.from,
+// 			text: message.text,
+// 			createdAt: new Date().getTime()
+// 		});
+// 	});
+// });
+
 io.on('connection', (socket) => {
-	console.log('New user connected');
+  console.log('New user connected');
 
-	socket.on('createMessage', (message) => {
-		console.log('createMessage', message);
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
 
-		// socket emits event to a single connection
-		// io will emit them to every connection
-		io.emit('newMessage', {
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		});
-	});
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
 
