67da8bb0ea2dae8f1103cc2b71a1e236ec361036
Abhimanyu Pathania <abpindia1944@gmail.com>
2017-02-12 19:29:40 +0530

    update public path

--- server/server.js
+++ server/server.js
@@ -3,10 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-
+const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
-const publicPath = path.join(__dirname + '/../public');
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
