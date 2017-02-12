f4cf999e1a8af10859d9055d40fbe4c6e7e8e45b
Abhimanyu Pathania <abpindia1944@gmail.com>
2017-02-13 00:50:48 +0530

    Add form for messages and show incoming message in ui

--- public/js/index.js
+++ public/js/index.js
@@ -6,4 +6,18 @@ socket.on('connect', function () {
 
 socket.on('newMessage', function (message) {
 	console.log('New Message', message);
+
+	var li = $(`<li>${message.from}: ${message.text}</li>`);
+	$('#messages').append(li);
 });
+
+$('#message-form').on('submit', function(e) {
+	e.preventDefault();
+
+	socket.emit('createMessage', {
+		from: 'User',
+		text: $('[name=message]').val()
+	}, function () {
+		// acknowledge callback
+	});
+})
